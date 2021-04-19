bf3b5bc0361574ec12d1125a81a189c5d935f7ee
tsipkens <tsipkens@gmail.com>
2021-04-19 15:35:32 -0400

    Separate util.py
    + Refactor main.py style.

--- py/main.py
+++ py/main.py
@@ -3,23 +3,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-import tfer_pma # import relevant functions to evaluate the PMA transfer function
+import tfer_pma  # import relevant functions to evaluate the PMA transfer function
 
 
 # define input variables
 m_star = 0.01e-18
-m = np.arange(0.8,1.2,0.001) * m_star # numpy array spanning 80 to 120% m_star
-d = None # mobility diameter (none uses mass-mobility relation)
-z = 1. # integer charge state
+m = np.arange(0.8, 1.2, 0.001) * m_star  # numpy array spanning 80 to 120% m_star
+d = None  # mobility diameter (none uses mass-mobility relation)
+z = 1.0  # integer charge state
 
 
 # B,Zp,_ = tfer_pma.tfer_1S(m_star,m,d,z,prop)
-prop = tfer_pma.prop_pma() # get default PMA properties
+prop = tfer_pma.prop_pma()  # get default PMA properties
 
 # Modify some of the properties,
 # in this case for the mass-mobility relation.
-rho_eff = 900; # effective density
-prop['m0'] = rho_eff * np.pi / 6 * 1e-27; # copy mass-mobility relation info (only used to find Rm)
+rho_eff = 900  # effective density
+prop['m0'] = rho_eff * np.pi / 6 * 1e-27 # copy mass-mobility relation info (only used to find Rm)
 prop['Dm'] = 3
 
 # prop['omega_hat'] = 1; # ratio of angular speeds (CPMA < 1 vs APM = 1)
@@ -29,33 +29,31 @@ sp,_ = tfer_pma.get_setpoint(prop, 'm_star', m_star, 'Rm', 10)
 
 
 # evaluate the transfer functions
-Lambda_1S,_ = tfer_pma.tfer_1S(sp, m, d, z, prop)
-Lambda_1C,_ = tfer_pma.tfer_1C(sp, m, d, z, prop)
-Lambda_1C_diff,_ = tfer_pma.tfer_1C_diff(sp, m, d, z, prop)
-if prop['omega_hat']==1:
-    Lambda_W1,_ = tfer_pma.tfer_W1(sp, m, d, z, prop)
-    Lambda_W1_diff,_ = tfer_pma.tfer_W1_diff(sp, m, d, z, prop)
+Lambda_1S, _ = tfer_pma.tfer_1S(sp, m, d, z, prop)
+Lambda_1C, _ = tfer_pma.tfer_1C(sp, m, d, z, prop)
+Lambda_1C_diff, _ = tfer_pma.tfer_1C_diff(sp, m, d, z, prop)
+if prop['omega_hat'] == 1:
+    Lambda_W1, _ = tfer_pma.tfer_W1(sp, m, d, z, prop)
+    Lambda_W1_diff, _ = tfer_pma.tfer_W1_diff(sp, m, d, z, prop)
 
 # plot the various transfer functions
 plt.plot(m, Lambda_1S)
 plt.plot(m, Lambda_1C)
 plt.plot(m, Lambda_1C_diff)
-if prop['omega_hat']==1:
+if prop['omega_hat'] == 1:
     plt.plot(m, Lambda_W1)
     plt.plot(m, Lambda_W1_diff)
 plt.show()
 
 
-
 # generate second plot demonstrating multiple charging
 m123 = np.arange(0.6,3.4,0.001) * m_star
-Lambda_1C_z1,_ = tfer_pma.tfer_1C_diff(sp, m123, d, 1, prop)
-Lambda_1C_z2,_ = tfer_pma.tfer_1C_diff(sp, m123, d, 2, prop)
-Lambda_1C_z3,_ = tfer_pma.tfer_1C_diff(sp, m123, d, 3, prop)
+Lambda_1C_z1, _ = tfer_pma.tfer_1C_diff(sp, m123, d, 1, prop)
+Lambda_1C_z2, _ = tfer_pma.tfer_1C_diff(sp, m123, d, 2, prop)
+Lambda_1C_z3, _ = tfer_pma.tfer_1C_diff(sp, m123, d, 3, prop)
 
 plt.plot(m123, Lambda_1C_z1)
 plt.plot(m123, Lambda_1C_z2)
 plt.plot(m123, Lambda_1C_z3)
-plt.plot(m123, Lambda_1C_z1 + Lambda_1C_z2 + Lambda_1C_z3, 'k--')
-    # different widths stem from resolution only applying to first peak
+plt.plot(m123, Lambda_1C_z1 + Lambda_1C_z2 + Lambda_1C_z3, 'k--')  # different widths stem from resolution only applying to first pea
 plt.show()
--- py/tfer_pma.py
+++ py/tfer_pma.py
@@ -5,6 +5,8 @@ import scipy.optimize
 import scipy.special
 import numpy as np
 
+import util
+
 
 
 #== TFER_1S ==============================================================#
@@ -250,9 +252,9 @@ def parse_inputs(sp, m, d=0, z=1, prop={}):
 
     if d==0 or d==None: # if mobility diameter is NOT specified
         print('Invoking mass-mobility relation to determine Zp.')
-        B,_,_ = mp2zp(m, z, prop['T'], prop['p'], prop)
+        B,_,_ = util.mp2zp(m, z, prop['T'], prop['p'], prop)
     else: # if mobility diameter is specified
-        B,_,_ = dm2zp(d, z, prop['T'], prop['p'])
+        B,_,_ = util.dm2zp(d, z, prop['T'], prop['p'])
 
     #-- Evaluate output parameters -------------------------------------------#
     tau = B * m
@@ -430,7 +432,7 @@ def get_setpoint(prop={}, *args):
         #-- Use definition of Rm to derive angular speed at centerline -------#
         #-- See Reavell et al. (2011) for resolution definition --#
         n_B = get_nb(sp['m_star'], prop)
-        B_star,_,_ = mp2zp(sp['m_star'], 1, prop['T'], prop['p'], prop)
+        B_star,_,_ = util.mp2zp(sp['m_star'], 1, prop['T'], prop['p'], prop)
 
         sp['m_max'] = sp['m_star'] * (1 / sp['Rm'] + 1)
         sp['omega'] = np.sqrt(prop['Q'] / (sp['m_star'] * B_star * 2 * np.pi * prop['rc'] ** 2 * prop['L'] * \
@@ -470,7 +472,7 @@ def get_resolution(m_star, omega, prop):
 
     n_B = get_nb(m_star, prop)
 
-    B_star,_,_ = mp2zp(m_star, 1, \
+    B_star,_,_ = util.mp2zp(m_star, 1, \
         prop['T'], prop['p'], prop) # mechanical mobility for z = 1
 
     t0 = prop['Q'] / (m_star * B_star * 2 * np.pi * prop['L'] * \
@@ -501,8 +503,8 @@ def get_nb(m_star, prop):
     m_high = m_star * 1.001 # perturb m_star up
     m_low  = m_star * .999 # perturb m_star down
 
-    B_high,_,_ = mp2zp(m_high, 1, prop['T'], prop['p'], prop)
-    B_low,_,_ = mp2zp(m_low, 1, prop['T'], prop['p'], prop)
+    B_high,_,_ = util.mp2zp(m_high, 1, prop['T'], prop['p'], prop)
+    B_low,_,_ = util.mp2zp(m_low, 1, prop['T'], prop['p'], prop)
 
     n_B = np.log10(B_high / B_low) / np.log10(m_high / m_low) # constant from Reveall et al.
 
@@ -510,130 +512,6 @@ def get_nb(m_star, prop):
 
     return n_B
 
-
-
-#== DM2ZP ================================================================#
-#   Function to calculate electric mobility from a vector of mobility diameter.
-#   Author:  Timothy Sipkens, 2019-10-22
-#
-# Inputs:
-#   d           Particle mobility diameter
-#   z           Integer charge state
-#   T           System temperature  (Optional)
-#   P           System pressure     (Optional)
-#
-# Outputs:
-#   Zp          Electromobility
-#   B           Mechanical mobility
-#
-# Notes:
-# 2 Some of the code is adapted from Buckley et al. (2017) and Olfert
-#   laboratory.
-#-------------------------------------------------------------------------#
-def dm2zp(d, z, T=0.0, p=0.0):
-
-    e = 1.6022e-19 # define electron charge [C]
-
-    if T==0.0 or p==0.0:
-        mu = 1.82e-5 # gas viscosity [Pa*s]
-        B = Cc(d) / (3 * np.pi * mu * d) # mechanical mobility
-    else:
-        S = 110.4 # temperature [K]
-        T_0 = 296.15 # reference temperature [K]
-        vis_23 = 1.83245e-5 # reference viscosity [kg/(m*s)]
-        mu = vis_23 * ((T / T_0) ** 1.5) * ((T_0 + S)/(T + S)) # gas viscosity
-            # Kim et al. (2005), ISO 15900, Eqn 3
-        B = Cc(d,T,p) / (3 * np.pi * mu * d) # mechanical mobility
-
-    Zp = B * e * z # electromobility
-
-    return B, Zp
-
-
-
-#== CC ===================================================================#
-#   Function to evaluate Cunningham slip correction factor.
-#   Author:  Timothy Sipkens, 2019-10-22
-#
-# Inputs:
-#   d           Particle mobility diameter
-#   T           System temperature  (Optional)
-#   P           System pressure     (Optional)
-#
-# Outputs:
-#   Zp          Electromobility
-#   B           Mechanical mobility
-#-------------------------------------------------------------------------#
-def Cc(d, T=0.0, p=0.0):
-
-    if T==0.0 or p==0.0: # if P and T are not specified, use Buckley/Davies
-        mfp = 66.5e-9 # mean free path
-
-        # for air, from Davies (1945)
-        A1 = 1.257
-        A2 = 0.4
-        A3 = 0.55
-
-    else: # from Olfert laboratory / Kim et al.
-        S = 110.4 # temperature [K]
-        mfp_0 = 6.730e-8 # mean free path of gas molecules in air [m]
-        T_0 = 296.15 # reference temperature [K]
-        p_0 = 101325 # reference pressure, [Pa] (760 mmHg to Pa)
-
-        p = p * p_0
-
-        mfp = mfp_0 * (T / T_0) ** 2 * (p_0 / p) * ((T_0 + S) / (T + S)) # mean free path
-            # Kim et al. (2005) (doi:10.6028/jres.110.005), ISO 15900 Eqn 4
-
-        A1 = 1.165
-        A2 = 0.483
-        A3 = 0.997 / 2
-
-
-    Kn = (2 * mfp) / d # Knudsen number
-    Cc = 1 + Kn * (A1 + A2 * np.exp(-(2 * A3) / Kn)) # Cunningham slip correction factor
-
-    return Cc
-
-
-
-#== MP2ZP ================================================================#
-#   Calculate electric mobility from a vector of particle mass.
-#   Author:   Timothy Sipkens, 2019-10-22
-#
-# Inputs:
-#   m           Particle mass
-#   z           Integer charge state
-#   T           System temperature
-#   P           System pressure
-#
-# Outputs:
-#   Zp          Electromobility
-#   B           Mechanical mobility
-#   d           Mobility diameter (simplied by mass-mobility relation)
-#
-# Note:
-#   Uses mass-mobility relationship to first convert to a mobility
-#   diameter and then estimates the mobility using dm2zp.
-#-------------------------------------------------------------------------#
-def mp2zp(m, z, T=0.0, P=0.0, prop={}):
-
-    #-- Parse inputs ---------------------------------------------------------#
-    if not bool(prop) or hasattr(prop, 'm0') or hasattr(prop, 'Dm'):
-         sys.exit('Please specify the mass-mobility relation parameters in prop.')
-             # if isempty or missing elements, produce error
-    #-------------------------------------------------------------------------#
-
-    d = 1e-9 * (m / prop['m0']) ** (1 / prop['Dm'])
-        # use mass-mobility relationship to get mobility diameter
-
-    #-- Use mobility diameter to get particle electro and mechanical mobl. ---#
-    if T==0.0 or P==0.0:
-        B, Zp = dm2zp(d,z)
-    else:
-        B, Zp = dm2zp(d,z,T,P)
-
-    return B, Zp, d
 
 
 
